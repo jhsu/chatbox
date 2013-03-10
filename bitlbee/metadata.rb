2490d0c836adaf47b4cb118caa848ec70ae73a19
Joseph Hsu <jhsu.x1@gmail.com>
2013-03-10 12:16:49 -0400

    ingredients and placeholders

---
+++ bitlbee/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       'Joseph Hsu'
+maintainer_email 'jhsu@josephhsu.com'
+license          'MIT'
+description      'Install bitlbee'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
+version          '0.0.1'
+depends 'ingredients'
---
+++ znc/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       'Joseph Hsu'
+maintainer_email 'jhsu@josephhsu.com'
+license          'MIT'
+description      'Install ZNC'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
+version          '0.0.1'
+depends 'ingredients'
